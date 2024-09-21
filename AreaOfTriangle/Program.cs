f44406d7328bfb4bc75845771399ee58effed0b1
Grace Cornel <202380082@psu.palawan.edu.ph>
2024-09-21 17:20:18 +0800

    Initial commit

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AreaOfTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //perimeter
+            Console.WriteLine("Length of A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Length of B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Length of C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+            //half of perimeter
+            double s = (a+b+c)/2;
+            Console.WriteLine("Half of Perimeter: "+s);
+
+            //area
+            double area = Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+
+            Console.WriteLine("This is the Area of the Triangle: "+area);
+        }
+    }
+}
---
+++ LeapYear/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0))
+            {
+                Console.WriteLine($"The year {year} is a leap year.");
+            }
+
+            else
+            {
+                Console.WriteLine($"The year {year} is not a leap year.");
+            
+            }
+                
+
+
+        }
+    }
+}
